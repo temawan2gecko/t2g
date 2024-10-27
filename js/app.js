810bbe77f61db32ccc5042d124bd6807ffeecad6
tenawan2gecko <artem.tchalunin@yandex.ru>
2024-10-27 13:30:28 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    effect: 'flip', // Используем эффект переворота
+    flipEffect: {
+        slideShadows: true,
+    },
+    speed: 1000,
+    on: {
+        slideChangeTransitionStart: function () {
+            const currentSlide = this.slides[this.activeIndex];
+            const targetPosition = currentSlide.offsetTop;
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth',
+            });
+        },
+    },
+});
+
+const text = "Hello World! I'm ";
+const name = "Chalunin Artem"; // Полное имя
+const fullText = text + name + "."; // Полный текст
+const textContainer = document.getElementById("animatedText");
+let index = 0;
+
+function typeText() {
+  // Генерируем случайное время задержки
+  const delay = Math.random() * 100 + 50; // От 50 до 150 мс
+
+  // Проверяем, достигли ли конца полного текста
+  if (index < fullText.length) {
+    if (index >= text.length && index < text.length + name.length) {
+      // Печатаем имя полностью красным цветом
+      textContainer.innerHTML += `<span class='red'>${name.charAt(
+        index - text.length
+      )}</span>`;
+    } else {
+      textContainer.innerHTML += fullText.charAt(index);
+    }
+    index++;
+  } else {
+    // Когда закончится полный текст, очищаем и начинаем снова
+    setTimeout(() => {
+      textContainer.innerHTML = ""; // Очищаем текст
+      index = 0; // Сбрасываем индекс
+      typeText(); // Запускаем снова
+    }, 1000); // Задержка перед перезапуском
+    return; // Выходим из функции
+  }
+
+  // Задаем случайную задержку для следующего символа
+  setTimeout(typeText, delay);
+}
+
+// Запускаем функцию, когда DOM загружен
+document.addEventListener("DOMContentLoaded", typeText);
